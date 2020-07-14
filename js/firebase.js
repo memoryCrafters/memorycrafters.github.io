8138f4bab5937ce6246384262bf802ef049044d4
Ethan Cooley <ethancooley17@gmail.com>
2020-07-14 11:23:01 -0400

    Update 7.14- Removed firebase authentication key

---
+++ js/firebase.js
@@ -0,0 +1,34 @@
+// Firebase configuration
+// Important: Removed configuration as repo is public
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
+// Select form and email input
+const form = document.querySelector('.form-inline');
+const inputEmail = form.querySelector('#inputEmail');
+
+// Function to push email to Firebase
+function firebasePush(input) {
+    //push itself
+    var emailPush = firebase.database().ref('Emails').push().set(
+        {
+            Email: input.value
+        }
+    );
+}
+
+// Run function on submit
+if (form) {
+    form.addEventListener('submit', function (evt) {
+        location.reload();
+        evt.preventDefault();
+        firebasePush(inputEmail);
+        
+
+        // Alert if everything runs.
+        return alert('Data Successfully Sent to Database');
+    })
+}
+
---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Pre-load images for site optimization
+
+var images = new Array();
+
+function preloadImages() {
+    for (i = 0; i < preloadImages.arguments.length; i++) {
+        images[i] = new Image();
+        images[i].src = preloadImages.arguments[i];
+    }
+}
+
+preloadImages("img/mC_favicon.JPG", "img/smartmockups_kcgjf0hg.jpg", "img/smartmockups_kbwarym5.png", "img/smartmockups_kbwaxz99.png", "img/smartmockups_kbwebwdr.png", "img/smartmockups_kby45upn.png");
+
