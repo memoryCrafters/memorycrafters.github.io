e730882aa3c91e48469f5d41688b1cae576267be
Ethan Cooley <ethancooley17@gmail.com>
2020-07-30 13:00:38 -0400

    Added mission page 7.30

--- js/fbauth.js
+++ js/fbauth.js
@@ -9,7 +9,7 @@ var uiConfig = {
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        //firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
         //firebase.auth.PhoneAuthProvider.PROVIDER_ID,
         //firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
     ],
