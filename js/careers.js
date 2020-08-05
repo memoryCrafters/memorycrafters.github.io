961b49a72398fa0052ae058a0f4293a68cc076ea
Ethan Cooley <ethancooley17@gmail.com>
2020-08-05 18:43:47 -0400

    8.5

--- js/careers.js
+++ js/careers.js
@@ -3,29 +3,37 @@
 const strategy = document.getElementById('strategy');
 const communications = document.getElementById('communications');
 const productManagement = document.getElementById('productManagement');
-const software = document.getElementById('software');
+const softwareIntern = document.getElementById('softwareIntern');
+const softwareEngineer = document.getElementById('softwareEngineer');
 const careerWrapper = document.getElementById('careerWrapper');
 
-var strategyListing = '<iframe src="https://drive.google.com/file/d/1MckynBICJAKhaKZR9rr1EvauYmbWSo_m/preview"></iframe>';
-var softwareListing = '<iframe src="https://drive.google.com/file/d/1k4pLXErjIH1Trucno3ZDiCCMohh7ML2i/preview"></iframe>';
+var businessListing = '<iframe src="https://drive.google.com/file/d/1MckynBICJAKhaKZR9rr1EvauYmbWSo_m/preview"></iframe>';
+var communicationsListing = '<iframe src="https://drive.google.com/file/d/1k4pLXErjIH1Trucno3ZDiCCMohh7ML2i/preview"></iframe>';
+var productListing = '<iframe src="https://drive.google.com/file/d/19QYTApE1xAflsPLKtGsNHHCiuZbDj9ck/preview"></iframe>';
+var softwareInternListing = '<iframe src="https://drive.google.com/file/d/1PgrEN74S5pGfiS0NFBcvw2bKi6Vvnl4f/preview"></iframe>';
+var softwareEngineerListing = '<iframe src="https://drive.google.com/file/d/1hlfSD_HU0g36hsK2aqOoQrVea9hPlys9/preview"></iframe>';
 
-// PM and Communications Listings Coming Soon
+// Communications Listing Coming Soon
 
 // Add listeners for each button
 
 strategy.addEventListener('click', function () {
-    careerWrapper.innerHTML = strategyListing;
+    careerWrapper.innerHTML = businessListing;
 });
 
 // communications.addEventListener('click', function () {
-//     careerWrapper.innerHTML = mayNews;
+//     careerWrapper.innerHTML = communicationsListing;
 // });
 
-// productManagement.addEventListener('click', function () {
-//     careerWrapper.innerHTML = mayNews;
-// });
+productManagement.addEventListener('click', function () {
+    careerWrapper.innerHTML = productListing;
+});
 
-software.addEventListener('click', function () {
-    careerWrapper.innerHTML = softwareListing;
+softwareIntern.addEventListener('click', function () {
+    careerWrapper.innerHTML = softwareInternListing;
+});
+
+softwareEngineer.addEventListener('click', function () {
+    careerWrapper.innerHTML = softwareEngineerListing;
 });
 
