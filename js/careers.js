976d352c19da48e0d2c4c566179e180367c1d5f0
Ethan Cooley <ethancooley17@gmail.com>
2020-08-12 16:20:14 -0400

    8.12

--- js/careers.js
+++ js/careers.js
@@ -1,39 +1,68 @@
 // Same as news-- Function to show embedded job listings
 
 const strategy = document.getElementById('strategy');
-const communications = document.getElementById('communications');
 const productManagement = document.getElementById('productManagement');
 const softwareIntern = document.getElementById('softwareIntern');
 const softwareEngineer = document.getElementById('softwareEngineer');
+
 const careerWrapper = document.getElementById('careerWrapper');
 
-var businessListing = '<iframe src="https://drive.google.com/file/d/1MckynBICJAKhaKZR9rr1EvauYmbWSo_m/preview"></iframe>';
-var communicationsListing = '<iframe src="https://drive.google.com/file/d/1k4pLXErjIH1Trucno3ZDiCCMohh7ML2i/preview"></iframe>';
-var productListing = '<iframe src="https://drive.google.com/file/d/19QYTApE1xAflsPLKtGsNHHCiuZbDj9ck/preview"></iframe>';
-var softwareInternListing = '<iframe src="https://drive.google.com/file/d/1PgrEN74S5pGfiS0NFBcvw2bKi6Vvnl4f/preview"></iframe>';
-var softwareEngineerListing = '<iframe src="https://drive.google.com/file/d/1hlfSD_HU0g36hsK2aqOoQrVea9hPlys9/preview"></iframe>';
+// Job Listings
+var businessListing = {
+    iframe : '<iframe src="https://drive.google.com/file/d/1MckynBICJAKhaKZR9rr1EvauYmbWSo_m/preview"></iframe>',
+    link : 'https://drive.google.com/file/d/1MckynBICJAKhaKZR9rr1EvauYmbWSo_m/view'
+};
 
-// Communications Listing Coming Soon
+var softwareEngineerListing = {
+    iframe : '<iframe src="https://drive.google.com/file/d/1hlfSD_HU0g36hsK2aqOoQrVea9hPlys9/preview"></iframe>',
+    link : 'https://drive.google.com/file/d/1hlfSD_HU0g36hsK2aqOoQrVea9hPlys9/view'
+};
 
-// Add listeners for each button
+var productInternListing = {
+    iframe : '<iframe src="https://drive.google.com/file/d/19QYTApE1xAflsPLKtGsNHHCiuZbDj9ck/preview"></iframe>',
+    link : 'https://drive.google.com/file/d/19QYTApE1xAflsPLKtGsNHHCiuZbDj9ck/view'
+};
 
-strategy.addEventListener('click', function () {
-    careerWrapper.innerHTML = businessListing;
+var softwareInternListing = {
+    iframe : '<iframe src="https://drive.google.com/file/d/1PgrEN74S5pGfiS0NFBcvw2bKi6Vvnl4f/preview"></iframe>',
+    link : 'https://drive.google.com/file/d/1PgrEN74S5pGfiS0NFBcvw2bKi6Vvnl4f/view'
+};
+
+// Function to hide embedded PDF on small devices (landscape/portrait phones)
+const btnFunction = function (element, listing) {
+
+    if (window.innerWidth < 768) {
+        careerWrapper.style.display = 'none';
+        element.onclick = function () {
+            window.open(listing.link);
+        };
+    }
+
+    else {
+        careerWrapper.style.display = 'block';
+        element.onclick = function () {
+            careerWrapper.innerHTML = listing.iframe;
+        };
+    };
+
+};
+
+// Hide embedded PDF if on small screen and instead pop-up preview. //
+
+// Initialize on page load
+if (window.innerWidth < 768) { newsWrapper.style.display = 'none' };
+
+btnFunction(strategy, businessListing);
+btnFunction(softwareEngineer, softwareEngineerListing);
+btnFunction(productManagement, productInternListing);
+btnFunction(softwareIntern, softwareInternListing);
+
+// When resizing 
+window.addEventListener('resize', function () {
+
+    btnFunction(strategy, businessListing);
+    btnFunction(softwareEngineer, softwareEngineerListing);
+    btnFunction(productManagement, productInternListing);
+    btnFunction(softwareIntern, softwareInternListing);
+
 });
-
-// communications.addEventListener('click', function () {
-//     careerWrapper.innerHTML = communicationsListing;
-// });
-
-productManagement.addEventListener('click', function () {
-    careerWrapper.innerHTML = productListing;
-});
-
-softwareIntern.addEventListener('click', function () {
-    careerWrapper.innerHTML = softwareInternListing;
-});
-
-softwareEngineer.addEventListener('click', function () {
-    careerWrapper.innerHTML = softwareEngineerListing;
-});
-
--- js/news.js
+++ js/news.js
@@ -3,17 +3,57 @@
 const july = document.getElementById('news-button1');
 const june = document.getElementById('news-button2');
 const may = document.getElementById('news-button3');
+
 const newsWrapper = document.getElementById('newswrapper');
 
-var juneNews = '<iframe src="https://drive.google.com/file/d/1XCa8DHH_UZv2nS2_vQ8LVBYk_v4Jce9-/preview"></iframe>';
-var mayNews = '<iframe src="https://drive.google.com/file/d/1BzEBHmVSIuNhRkYbC-4gvWDv3djxgBsE/preview"></iframe>';
+var julyNews = {
+    iframe : '<iframe src="https://drive.google.com/file/d/19ZtZ9S9Vx_uYGdhoGOUuPLid3E1vTw-u/preview"></iframe>',
+    link : 'https://drive.google.com/file/d/19ZtZ9S9Vx_uYGdhoGOUuPLid3E1vTw-u/view'
+};
 
-// July newsletter coming soon
+var juneNews = {
+    iframe : '<iframe src="https://drive.google.com/file/d/1XCa8DHH_UZv2nS2_vQ8LVBYk_v4Jce9-/preview"></iframe>',
+    link : 'https://drive.google.com/file/d/1XCa8DHH_UZv2nS2_vQ8LVBYk_v4Jce9-/view'
+};
 
-june.addEventListener('click', function () {
-    newsWrapper.innerHTML = juneNews;
-});
+var mayNews = {
+    iframe : '<iframe src="https://drive.google.com/file/d/1BzEBHmVSIuNhRkYbC-4gvWDv3djxgBsE/preview"></iframe>',
+    link : 'https://drive.google.com/file/d/1BzEBHmVSIuNhRkYbC-4gvWDv3djxgBsE/view'
+};
 
-may.addEventListener('click', function () {
-    newsWrapper.innerHTML = mayNews;
+// Function to hide embedded PDF on small devices (landscape/portrait phones)
+const btnFunction = function (element, newsMonth) {
+
+    if (window.innerWidth < 768) {
+        newsWrapper.style.display = 'none';
+        element.onclick = function () {
+            window.open(newsMonth.link);
+        };
+    }
+
+    else {
+        newsWrapper.style.display = 'block';
+        element.onclick = function () {
+            newsWrapper.innerHTML = newsMonth.iframe;
+        };
+    };
+
+};
+
+// Hide embedded PDF if on small screen and instead pop-up preview. //
+
+// Initialize on page load
+if (window.innerWidth < 768) { newsWrapper.style.display = 'none' };
+
+btnFunction(july, julyNews);
+btnFunction(june, juneNews);
+btnFunction(may, mayNews);
+
+// When resizing 
+window.addEventListener('resize', function () {
+
+    btnFunction(july, julyNews);
+    btnFunction(june, juneNews);
+    btnFunction(may, mayNews);
+
 });
